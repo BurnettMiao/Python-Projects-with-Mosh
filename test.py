9946eff7be96d24dea893290a4578956f2fb3b6d
Burnett Miao <searchfor1987@gmail.com>
2024-09-21 11:44:27 +0800

    14_word_guessing_game finish

--- test.py
+++ test.py
@@ -1,40 +1,33 @@
-import os
+# def read_words():
+#     try:
+#         with open('words.txt', 'r') as file:
+#             words = file.read().splitlines()
+#             return words
+#     except FileExistsError:
+#         print('words.txt does not exist.')
+#         return []
 
-# def main():
-#     directory = r'C:\Users\burnett\Desktop\a發音'
-#     file_count = 0  # 初始化檔案數量的計數器
-    
-#     for filename in os.listdir(directory):
-#         f = os.path.join(directory, filename)
-#         if os.path.isfile(f):
-#             file_count += 1  # 每當找到一個檔案，計數器加一
+# def read_words():
+#     try:
+#         with open('words.txt', 'r') as file:
+#             words = file.read().splitlines()
+#             return words
+#     except FileExistsError:
+#         print('Words.txt does not exist.')
+#         return []
 
-#     print(f"資料夾裡有 {file_count} 個檔案")
-
-
-# def main():
-#     directory = r'C:\Users\burnett\Desktop\a發音'
-#     file_count = 0
-
-#     for dirpath, dirname, filenames in os.walk(directory):
-#         print(f"1 {dirpath}")
-#         print(f"2 {dirname}")
-#         print(f"3 {filenames}")
-#         file_count += len(filenames)
-    
-#     print(f"資料夾裡面有{file_count}個檔案")
-
-# def main():
-#     directory = r'C:\Users\burnett\Desktop\a發音'
-
-#     for filename in os.listdir(directory):
-#         file_path = os.path.join(directory, filename)
-#         if os.path.isfile(file_path):
-#             file_name_without_extension, extension = os.path.splitext(filename)
-#             print(file_name_without_extension)
+def read_words():
+    try:
+        with open('word.txt', 'r') as file:
+            words = file.read().splitlines()
+            return words
+    except FileNotFoundError:
+        print('Words.txt does not exist.')
+        return []
 
 def main():
-    print(os.getcwd())
+    words = read_words()
+    print(words)
 
 
 if __name__ == '__main__':
