5e18acb07fad66e8f636a30955b6a3557c01e248
Burnett Miao <searchfor1987@gmail.com>
2024-10-25 10:06:32 +0800

    rename folder

--- practice.py
+++ practice.py
@@ -18,19 +18,37 @@
 
 
 # practice
-def read_words():
-    try:
-        with open('word.txt', 'r') as file:
-            words = file.read().splitlines()
-            return words
-    except FileNotFoundError:
-        print('Words.txt does not exist.')
-        return []
+# def read_words():
+#     try:
+#         with open('word.txt', 'r') as file:
+#             words = file.read().splitlines()
+#             return words
+#     except FileNotFoundError:
+#         print('Words.txt does not exist.')
+#         return []
+
+# def main():
+#     words = read_words()
+#     print(words)
+
+
+# if __name__ == '__main__':
+#     main()
 
 def main():
-    words = read_words()
-    print(words)
+    try:
+        value = int(input('請輸入一個數字: '))
+        result = 10 / value
+        print(result)
+    except Exception as e:
+        print(f'發生錯誤: {e}')
 
+
+# def main():
+#     value = int(input('請輸入一個數字: '))
+#     result = 10 / value
+#     print(result)
+    
 
 if __name__ == '__main__':
     main()
