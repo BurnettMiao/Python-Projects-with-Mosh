3e4a00d9b08e96ad2702f82e7e8ad2feb11ecd80
Burnett Miao <searchfor1987@gmail.com>
2024-10-30 16:22:52 +0800

    新增個人圖書館app

--- practice.py
+++ practice.py
@@ -1,7 +1,9 @@
 library = {
-    '我是書名': {'author': '我是作者', 'is_read': False}
+    '我是書名': {'author': '我是作者', 'is_read': False},
+    'book one': {'author': 'author', 'is_read': False},
 }
 
+# 顯示選單
 def show_menu():
     print("\n===== 個人圖書館 =====")
     print("1. 新增書籍")
@@ -10,6 +12,7 @@ def show_menu():
     print("4. 查看所有書籍")
     print("5. 退出")
 
+# 確認使用者輸入狀態
 def check_user_input(prompt):
     while True:
         name = input(prompt)
@@ -21,6 +24,7 @@ def check_user_input(prompt):
         name = " ".join(name.split())
         return name
 
+# 新增書籍
 def add_book():
     check_book_prompt = "輸入書名: "
     book_name = check_user_input(check_book_prompt)
@@ -29,23 +33,73 @@ def add_book():
     author = check_user_input(check_author_prompt)
     # print(author)
     library[book_name] = {'author': author, 'is_read': False}
-    print(library)
 
+    print(f"\n書名: {book_name}已新增")
+
+# 標記書籍為已讀
 def mark_as_read():
     check_book_prompt = "輸入要標記為已讀的書名: "
     book_name = check_user_input(check_book_prompt)
 
     if book_name in library:
         library[book_name]['is_read'] = True
-        print(f"{book_name}標記為已讀")
+        print(f"\n{book_name}標記為已讀")
     else:
-        print('找不到相關書名')
-    
+        print('\n找不到相關書名')
 
+# 刪除書籍
+def delete_book():
+    check_book_prompt = "輸入要刪除的書名: "
+    book_name = check_user_input(check_book_prompt)
 
-# show_menu()
-# add_book()
-mark_as_read()
+    if book_name in library:
+        del library[book_name]
+        print(f"\n{book_name}已刪除")
+    else:
+        print('\n找不到相關書籍')
+
+# 查看所有書籍
+def view_books():
+    if not library:
+        print('\n目前沒有收藏的書籍')
+    else:
+        print('===== 書籍清單 =====')
+        for name, details in library.items():
+            status = "已讀" if details['is_read'] else "未讀"
+            print(f"\n書名: {name}, 作者: {details['author']}, 狀態: {status}")
+
+# 確認使用者是否輸入正確操作數字
+def check_input_number():
+    operator_number = ('1', '2', '3', '4', '5')
+    while True:
+        user_input = input('\n選擇操作(1-5): ').strip()
+        if user_input not in operator_number:
+            print('請輸入正確數字(1-5)')
+            continue
+        else:
+            return user_input
+        
+# 主程式迴圈
+def main():
+    while True:
+        show_menu()
+        choice = check_input_number()
+        if choice == '1':
+            add_book()
+        elif choice == '2':
+            mark_as_read()
+        elif choice == '3':
+            delete_book()
+        elif choice == '4':
+            view_books()
+        else:
+            print("\n退出圖書館管理系統")
+            break
+
+if __name__ == '__main__':
+    main()
+
+        
 
 
 
